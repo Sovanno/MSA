81118bffb31c823568289870d2b3efda4cb1934c
Fink Eduard <fink.2004@mail.ru>
2025-10-22 19:10:00 +0600

    refactor: user controller

--- src/controllers/user_controller.py
+++ src/controllers/user_controller.py
@@ -12,26 +12,26 @@ def verify_password(plain_password, hashed_password):
 
 def create_user(db: Session, email: str, username: str, password, str):
     hashed_pwd = get_password_hash(password)
-    user = models.User(email=email, username=username, password=hashed_pwd)
+    user = models.user.User(email=email, username=username, password=hashed_pwd)
     db.add(user)
     db.commit()
     db.refresh(user)
     return user
 
 def authenticate_user(db: Session, email: str, password: str):
-    user = db.query(models.User).filter(models.User.email == email).first()
+    user = db.query(models.user.User).filter(models.user.User.email == email).first()
     if not user or not verify_password(password, user.password):
         return None
     return user
 
 def get_user_by_username(db: Session, username: str):
-    return db.query(models.User).filter(models.User.username == username).first()
+    return db.query(models.user.User).filter(models.user.User.username == username).first()
 
-def update_user(db: Session, user: models.User, email: str = None, username: str = None, bio: str = None, image: str = None, password: str = None):
+def update_user(db: Session, user: models.user.User, email: str = None, username: str = None, bio: str = None, image: str = None, password: str = None):
     if email:
         user.email = email
     if username:
-        user.username - username
+        user.username = username
     if bio is not None:
         user.bio = bio
     if image is not None:
