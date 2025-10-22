f71a169436e163f79c1f88f87fd6c32d7a91c66c
Fink Eduard <fink.2004@mail.ru>
2025-10-22 18:17:12 +0600

    feat: user controller

---
+++ src/controllers/user_controller.py
@@ -0,0 +1,43 @@
+from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+from src import models
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_user(db: Session, email: str, username: str, password, str):
+    hashed_pwd = get_password_hash(password)
+    user = models.User(email=email, username=username, password=hashed_pwd)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
+
+def authenticate_user(db: Session, email: str, password: str):
+    user = db.query(models.User).filter(models.User.email == email).first()
+    if not user or not verify_password(password, user.password):
+        return None
+    return user
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(models.User).filter(models.User.username == username).first()
+
+def update_user(db: Session, user: models.User, email: str = None, username: str = None, bio: str = None, image: str = None, password: str = None):
+    if email:
+        user.email = email
+    if username:
+        user.username - username
+    if bio is not None:
+        user.bio = bio
+    if image is not None:
+        user.image = image
+    if password:
+        user.password = get_password_hash(password)
+    db.commit()
+    db.refresh(user)
+    return user
